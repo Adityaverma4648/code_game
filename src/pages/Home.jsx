bf04ebf45669f737af4472af0d263c3c7ec4faa9
Adityaverma4648 <adityaverma4648@gmail.com>
2023-09-25 09:10:39 +0530

    intialized a react app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+import React , {useState , useEffect} from 'react'
+
+const Home = () => {
+
+    const [data, setData] = useState([]);
+   
+    useEffect(()=>{
+  
+    },[])
+
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
